dd07e9a79a095b7f463b4459df98c2d86283dcac
Franco M <francomaro@gmail.com>
2025-12-07 02:40:41 -0300

    Fix

--- src/control/Curves.h
+++ src/control/Curves.h
@@ -1,22 +1,20 @@
 #pragma once
 class CVector;
+#define SA
 
 class CCurves
 {
 public:
 #ifdef SA
-	static float DistForLineToCrossOtherLine(float LineBaseX, float LineBaseY, float LineDirX, float LineDirY,
-		float OtherLineBaseX, float OtherLineBaseY, float OtherLineDirX, float OtherLineDirY);
-	static float CalcSpeedVariationInBend(const CVector& startCoors, const CVector& endCoors,
-		float StartDirX, float StartDirY, float EndDirX, float EndDirY);
-	static float CalcSpeedScaleFactor(const CVector& startCoors, const CVector& endCoors,
-		float StartDirX, float StartDirY, float EndDirX, float EndDirY);
-	static void CalcCurvePoint(const CVector& startCoors, const CVector& endCoors,
-		const CVector& startDir, const CVector& endDir, float Time, int32 TraverselTimeInMillis,
-		CVector& resultCoor, CVector& resultSpeed);
-	static float CalcCorrectedDist(float Current, float Total, float SpeedVariation, float* pInterPol);
+	static float DistForLineToCrossOtherLine(float LineBaseX, float LineBaseY, float LineDirX, float LineDirY, float OtherLineBaseX, float OtherLineBaseY,
+	                                         float OtherLineDirX, float OtherLineDirY);
+	static float CalcSpeedVariationInBend(CVector *startCoors, CVector *endCoors, float StartDirX, float StartDirY, float EndDirX, float EndDirY);
+	static float CalcSpeedScaleFactor(CVector *startCoors, CVector *endCoors, float StartDirX, float StartDirY, float EndDirX, float EndDirY);
+	static void CalcCurvePoint(CVector *startCoors, CVector *endCoors, CVector *startDir, CVector *endDir, float Time, int32 TraverselTimeInMillis,
+	                           CVector *resultCoor, CVector *resultSpeed);
+	static float CalcCorrectedDist(float Current, float Total, float SpeedVariation, float *pInterPol);
 #else
-	static float CalcSpeedScaleFactor(CVector*, CVector*, float, float, float, float);
-	static void CalcCurvePoint(CVector*, CVector*, CVector*, CVector*, float, int32, CVector*, CVector*);
+	static float CalcSpeedScaleFactor(CVector *, CVector *, float, float, float, float);
+	static void CalcCurvePoint(CVector *, CVector *, CVector *, CVector *, float, int32, CVector *, CVector *);
 #endif
 };
